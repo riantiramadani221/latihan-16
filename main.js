8b1efeb76831214a1617f14951bb7d75ff6fe3ff
rianti ramadani <riantiramadani221@gmail.com>
2025-11-18 11:09:43 +0700

    update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 //fungsi untuk mengecek kelulusan siswa
 function cekkelulusan() {
-  console.log('cek..')
   // ambil nilai dari elemen input
   let nilaiInput = Number(document.getElementById("nilai").value);
   
@@ -13,10 +12,29 @@ function cekkelulusan() {
   
   // validasi input kosong
   if (!nilaiInput || !hadirInput) {
-    hasil = "⚠️ Nilai dan kehadiran hadir diisi!"
+    hasil = "⚠️ Nilai dan kehadiran diisi!"
     
     document.getElementById("hasil").innerHTML = hasil;
-    
+    document.getElementById("hasil").style.color = "#c0392b"
     return
   }
+  // logika pengecekan kelulusan 
+  if (
+    (nilaiInput >= 90 && hadirInput >= 95) ||
+    (nilaiInput >= 95 && hadirInput >= 80) ||
+    (nilaiInput >= 75 && hadirInput >= 98)
+  ) {
+    // kelulusan dengan bonus 
+    hasil = "🎉 Lulus dengan Bonus"
+    document.getElementById("hasil").style.color = "#3498db"
+  } else if (nilaiInput >= 80 && hadirInput >= 85)
+  {
+    // kelulusan setandar 
+    hasil = "✅ lulus."
+  } else {
+    //tidak lulus 
+    hasil = "❌ tidak lulus."
+  }
+  // tampilkan hasil ke elemen dengan id hasil
+  document.getElementById("hasil").innerHTML = hasil
 }
